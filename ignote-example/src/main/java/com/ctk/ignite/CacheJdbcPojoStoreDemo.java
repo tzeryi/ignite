c0c1c65fae4e8186cec8caff32cf6f49d0cd4fd8
tzeryi <tzeryi@gmail.com>
2016-08-12 18:31:41 +0800

    fix bug

--- ignote-example/src/main/java/com/ctk/ignite/CacheJdbcPojoStoreDemo.java
+++ ignote-example/src/main/java/com/ctk/ignite/CacheJdbcPojoStoreDemo.java
@@ -1,17 +1,42 @@
 package com.ctk.ignite;
 
+import java.util.List;
+
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.query.QueryCursor;
+import org.apache.ignite.cache.query.SqlFieldsQuery;
+
+import com.ctk.ignite.vo.Person;
+import com.ctk.ignite.vo.PersonKey;
 
 public class CacheJdbcPojoStoreDemo {
 
 	public static void main(String[] args) {
+		load();
+		//query();
+	}
+
+	public static void load() {
 		try (Ignite ignite = Ignition.start("ignite-cache-store.xml")) {
-			IgniteCache<Integer, String> cache = ignite.getOrCreateCache("personCache");
+			IgniteCache<PersonKey, Person> cache = ignite.getOrCreateCache("personCache");
 			
 			cache.loadCache(null);
+
+            QueryCursor<List<?>> cursor = cache.query(new SqlFieldsQuery(
+                    "select * from Person where salary > 1000 and salary <= 300"));
+            System.out.println(cursor.getAll());
 		}
 	}
 
+	public static void query() {
+		try (Ignite ignite = Ignition.start("ignite-cache-store.xml")) {
+			IgniteCache<PersonKey, Person> cache = ignite.getOrCreateCache("personCache");
+			
+            QueryCursor<List<?>> cursor = cache.query(new SqlFieldsQuery(
+                    "select * from Person where salary > 1000 and salary <= 300"));
+            System.out.println(cursor.getAll());
+		}
+	}
 }
